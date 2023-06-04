033605073f46611a2ff5021eed74c50bb3c121e7
Jibon L. Costa <jiboncosta57@gmail.com>
2023-06-04 20:41:58 +0200

    updated depreciated code

--- install.sh
+++ install.sh
@@ -314,8 +314,8 @@ install_fonts() {
 
 prepare_recorder() {
   ## prepare chrome
-  curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-  echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >/etc/apt/sources.list.d/google-chrome.list
+  curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/googlechrome-linux-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/googlechrome-linux-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" >/etc/apt/sources.list.d/google-chrome.list
 
   ## prepare nodejs
   curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
